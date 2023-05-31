f867d81e90b16386edd0fd82ad158ba370abdfb9
Carlos Romao <carlos.romao@pentagro.com.br>
2023-05-30 23:22:23 -0300

    :sparkles: First Model-Controller-View - Department

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> departaments = new List<Departament>
+            {
+                new Departament { Id = 1, Name = "Eletronics" },
+                new Departament { Id = 2, Name = "Fashion" }
+            };
+
+            return View(departaments);
+        }
+    }
+}
