ceffb5bcb3ddd8652c412dee40d6b6b2fbdf74ed
Carlos Romao <carlos.romao@pentagro.com.br>
2023-06-17 20:34:55 -0300

    :sparkles: SellerService and basic FindAll

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<SellerService>();
 
 var app = builder.Build();
 
