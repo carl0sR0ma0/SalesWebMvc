6f50d43978bf63739911caece6e6e623e83942c7
Carlos Romao <carlos.romao@pentagro.com.br>
2023-06-19 23:17:17 -0300

    :sparkles: Implementing grouping search

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 ﻿using SalesWebMvc.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
@@ -16,7 +17,11 @@ namespace SalesWebMvc.Models
         }
 
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
 
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -27,5 +27,22 @@ namespace SalesWebMvc.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+        
+        public List<IGrouping<Department, SalesRecord>> FindByDateGrouping(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+                result = result.Where(x => x.Date >= minDate.Value);
+            if (maxDate.HasValue)
+                result = result.Where(x => x.Date <= maxDate.Value);
+
+            return result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .AsEnumerable()
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToList();
+        }
     }
 }
